683fe8d5724450da91c1dcde67f99f6e6db6c1cb
Faww <weedsm@mail.ru>
2022-03-23 11:25:20 +0300

    Initial Commit

---
+++ BirthdayTracker/Birthday.swift
@@ -0,0 +1,19 @@
+//
+//  Birthday.swift
+//  BirthdayTracker
+//
+//  Created by MacBook on 23.03.2022.
+//
+
+import Foundation
+
+class Birthday {
+let firstName: String
+    let lastName: String
+    let birthdate: Date
+    init(firstName: String, lastName: String, birthdate: Date) {
+    self.firstName = firstName
+        self.lastName = lastName
+        self.birthdate = birthdate
+    }
+}
